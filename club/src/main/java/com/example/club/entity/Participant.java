91cde317407c92d299d8acedebe43265e90cc239
hakki5665@gmail.com <hakki5665@gmail.com>
2026-07-26 21:29:18 +0500

    SecondCommitWithChanges

---
+++ club/src/main/java/com/example/club/entity/Participant.java
@@ -0,0 +1,30 @@
+package com.example.club.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "participants")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false, length = 100)
+    private String fullName;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "participant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QrCode> qrCodes = new ArrayList<>();
+}
