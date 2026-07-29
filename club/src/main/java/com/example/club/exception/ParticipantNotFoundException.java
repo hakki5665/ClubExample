56a900780a688c0992df88b1b80a5580ffd77d47
hakki5665@gmail.com <hakki5665@gmail.com>
2026-07-30 00:05:30 +0500

    fourthCommit

--- club/src/main/java/com/example/club/exception/ParticipantNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.club.exception;
-
-public class ParticipantNotFoundException extends ClubException {
-    public ParticipantNotFoundException() {
-        super("Участник не найден");
-    }
-}
--- club/src/main/java/com/example/club/exception/QrCodeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.club.exception;
-
-public class QrCodeNotFoundException extends ClubException {
-    public QrCodeNotFoundException() {
-        super("QR-код не найден или уже использован");
-    }
-}
